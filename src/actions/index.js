0b44d3ca44958612d49bec3cb6ee91ecd354a94b
Ivan <marionltan@gmail.com>
2019-11-04 15:41:47 +0800

    user can be displayed in post

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,13 @@ import jsonPlaceholder from '../apis/JSONPlaceholder';
 
 
 export const fetchPosts= ()=> async (dispatch)=>{
-		const response = await jsonPlaceholder.get('/posts');
-	
-		dispatch({type: 'FETCH_POSTS', payload:response.data})
-	};
+	const response = await jsonPlaceholder.get('/posts');
+
+	dispatch({type: 'FETCH_POSTS', payload:response.data})
+};
+
+export const fetchUser = id => async dispatch=>{
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({type: 'FETCH_USER', payload:response.data});
+};
